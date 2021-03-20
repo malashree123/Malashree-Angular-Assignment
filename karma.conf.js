8dc6d55f681e4122ad700bf1057763c482c1b545
malashree koley <malashreekoley542@gmail.com>
2021-03-21 00:12:45 +0530

    1st commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/sumit'),
+      dir: require('path').join(__dirname, './coverage/malashree'),
       subdir: '.',
       reporters: [
         { type: 'html' },
